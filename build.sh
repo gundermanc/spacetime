17d2eb58b2e7f1fa61d2554e21af0a9c705ca8c7
Christian Gunderman <gundermanc@gmail.com>
2019-09-18 00:10:02 -0700

    Switch over to ricebox.

--- build.sh
+++ build.sh
@@ -1,1 +1,9 @@
+# Build...
+echo "Building..."
 go build -v ./cmd/spacetime
+
+# Embed assets...
+echo "Embedding assets..."
+pushd server
+go run github.com/GeertJohan/go.rice/rice embed-go
+popd
--- init.sh
+++ init.sh
@@ -6,10 +6,3 @@ yarn
 echo "Downloading dependencies..."
 go get -v -t -d ./...
 echo
-
-# Generate embedded templates.
-echo "Generating embedded templates..."
-rm -rf server/generated
-pushd server/http
-go generate
-popd
