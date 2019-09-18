5c94123e7bef15d4fb4daa31eb8dd474ba96102d
Christian Gunderman <gundermanc@gmail.com>
2019-09-18 00:16:27 -0700

    Fix build

--- init.sh
+++ init.sh
@@ -5,3 +5,7 @@ yarn || exit $?
 # Download Go code dependencies.
 echo "Downloading dependencies..."
 go get -v -t -d ./... || exit $?
+
+# TODO: are these redundant?
+go get github.com/GeertJohan/go.rice || exit $?
+go get github.com/GeertJohan/go.rice/rice || exit $?
